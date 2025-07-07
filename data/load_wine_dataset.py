a0fb0965080191172d564c6c44739820b07cc7fa
AngelLagr <119140988+AngelLagr@users.noreply.github.com>
2025-07-07 14:18:07 -0500

    Refactor : remove useless informations and useless parameters

--- data/load_wine_dataset.py
+++ data/load_wine_dataset.py
@@ -22,12 +22,12 @@ def load_wine_dataset(test_size=0.2, random_state=42):
     df['target'] = data.target
 
     # Split the data into features and labels
-    X = df.drop('target', axis=1)
+    x = df.drop('target', axis=1)
     y = df['target']
 
     # Normalize the features
     scaler = StandardScaler()
-    x_scaled = scaler.fit_transform(X)
+    x_scaled = scaler.fit_transform(x)
 
     # Split the dataset into training and testing sets
     x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=test_size, random_state=random_state)
